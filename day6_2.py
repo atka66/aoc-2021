b850fad0c12319635469a79da73fab9a7945469e
Attila Fekete <attila.fekete@enghouse.com>
2022-01-07 14:43:57 +0100

    day6 but actually good

--- day6_2.py
+++ day6_2.py
@@ -1,24 +1,8 @@
 # read input file
-from collections import defaultdict
+import numpy
 
-
-file = open('input/day6.txt', 'r')
-input = file.readlines()
-
-dayMap = defaultdict(int)
-
-def iterate():
-    for i in range(0, 9):
-        dayMap[i-1] = dayMap[i]
-    dayMap[6] += dayMap[-1]
-    dayMap[8] = dayMap[-1]
-
-# body of solution
-for i in list(map(int, input[0].strip().split(','))):
-    dayMap[i] += 1
-
+dayMap = numpy.bincount(numpy.fromfile('input/day6.txt', dtype=int, sep=','), minlength=9)
 for i in range(256):
-    iterate()
-
-del dayMap[-1]
-print(sum(dayMap.values()))
+    dayMap = numpy.roll(dayMap, -1)
+    dayMap[6] += dayMap[8]
+print(dayMap.sum())
